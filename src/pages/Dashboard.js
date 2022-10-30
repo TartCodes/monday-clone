646218de264b6643bf25556dad603f6e8c60327b
TartCodes <tartcodes@gmail.com>
2022-10-30 16:36:34 -0400

    link added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,34 @@
 import TicketCard from '../components/TicketCard'
 
 const Dashboard = () => {
+
+  const tickets = [
+    {
+      category: 'Q1 2022',
+      color : 'red',
+      title: 'NFT Safety 101 Video',
+      owner: 'Matt T',
+      avatar: 'https://media-exp1.licdn.com/dms/image/C5603AQEHt6GbaiXpKg/profile-displayphoto-shrink_400_400/0/1517498119779?e=1672876800&v=beta&t=VXHxMhhDzb4XFVBvIAzhyKKR3qxYUYJKlbz95u5WuLE',
+      status: 'done',
+      priority: 5,
+      progress: 40,
+      descriptions: 'Make a video to showcase how to work with NFTs blah blah blah blah',
+      timestamp: '2022-10-30T16:34'
+    },
+    {
+      category: 'Q1 2022',
+      color : 'red',
+      title: 'Build and sell AI Model',
+      owner: 'Matt T',
+      avatar: 'https://media-exp1.licdn.com/dms/image/C5603AQEHt6GbaiXpKg/profile-displayphoto-shrink_400_400/0/1517498119779?e=1672876800&v=beta&t=VXHxMhhDzb4XFVBvIAzhyKKR3qxYUYJKlbz95u5WuLE',
+      status: 'working on it',
+      priority: 2,
+      progress: 70,
+      descriptions: 'Make a video to work and make AIs blah blah blah blah',
+      timestamp: '2022-10-30T16:34'
+    }
+  ]
+
   return (
     <div className="dashboard">
       <h1>My Projects</h1>
