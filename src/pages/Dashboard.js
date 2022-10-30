405cb008cd973ab74af2ff07356f0b38d761c63b
TartCodes <tartcodes@gmail.com>
2022-10-30 18:03:52 -0400

    adding data

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -26,15 +26,36 @@ const Dashboard = () => {
       progress: 70,
       descriptions: 'Make a video to work and make AIs blah blah blah blah',
       timestamp: '2022-10-30T16:34'
+    },
+    {
+      category: 'Q2 2022',
+      color : 'blue',
+      title: 'Build a bot',
+      owner: 'Matt T',
+      avatar: 'https://media-exp1.licdn.com/dms/image/C5603AQEHt6GbaiXpKg/profile-displayphoto-shrink_400_400/0/1517498119779?e=1672876800&v=beta&t=VXHxMhhDzb4XFVBvIAzhyKKR3qxYUYJKlbz95u5WuLE',
+      status: 'working on it',
+      priority: 3,
+      progress: 10,
+      descriptions: 'Make a video around a bot blah blah blah blah',
+      timestamp: '2022-10-30T16:34'
     }
   ]
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category ))
+  ]
+  console.log(uniqueCategories)
 
   return (
     <div className="dashboard">
       <h1>My Projects</h1>
       <div className="ticket-container">
-        <TicketCard/>
-      </div>
+        {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div key={categoryIndex}>
+            <h3>{uniqueCategory}</h3>
+          </div>
+        ))}
+        </div>      
     </div>
   )
 }
