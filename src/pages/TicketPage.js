ad7c08797872b3479b0a5ac68b384615e6aa5ca6
TartCodes <tartcodes@gmail.com>
2022-10-31 13:09:40 -0400

    started inputs

--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, version } from "react";
 
 const TicketPage = () => {
   const [formData, setFormData] = useState({
@@ -22,6 +22,8 @@ const TicketPage = () => {
     }));
   };
 
+  const categories = ["test1", "test2"];
+
   console.log(formData);
 
   return (
@@ -39,6 +41,133 @@ const TicketPage = () => {
               required={true}
               value={formData.title}
             />
+
+            <label htmlFor="description">Description</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.description}
+            />
+
+            <label>Category</label>
+            <select
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+            >
+              {categories?.map((category, _index) => (
+                <option key={_index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+
+            <label htmlFor="new-category">Description</label>
+            <input
+              id="new-category"
+              name="category"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.category}
+            />
+
+            <label>Priority</label>
+            <div className="multiple-input-container">
+              <input
+                id="priority-1"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={1}
+                checked={formData.priority === 1}
+              />
+              <label htmlFor="priority-1"></label>
+
+              <input
+                id="priority-2"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={2}
+                checked={formData.priority === 2}
+              />
+              <label htmlFor="priority-2"></label>
+
+              <input
+                id="priority-3"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={3}
+                checked={formData.priority === 3}
+              />
+              <label htmlFor="priority-3"></label>
+
+              <input
+                id="priority-4"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={4}
+                checked={formData.priority === 4}
+              />
+              <label htmlFor="priority-4"></label>
+
+              <input
+                id="priority-5"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={5}
+                checked={formData.priority === 5}
+              />
+              <label htmlFor="priority-5"></label>
+            </div>
+
+            {editMode && (
+              <>
+                <input
+                  type="range"
+                  name="progress"
+                  id="progress"
+                  value={formData.progress}
+                  min="0"
+                  max="100"
+                  onChange={handleChange}
+                />
+                <label htmlFor="progress">Progress</label>
+
+                <label>Status</label>
+                <select
+                  name="status"
+                  value={formData.status}
+                  onChange={handleChange}
+                >
+                  <option selected={formData.status === "done"} value="done">
+                    Done
+                  </option>
+                  <option
+                    selected={formData.status === "working on it"}
+                    value="working on it"
+                  >
+                    Working on it
+                  </option>
+                  <option selected={formData.status === "stuck"} value="stuck">
+                    Stuck
+                  </option>
+                  <option
+                    selected={formData.status === "not started"}
+                    value="not started"
+                  >
+                    Not started
+                  </option>
+                </select>
+              </>
+            )}
           </section>
         </form>
       </div>
